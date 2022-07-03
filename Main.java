5515a3d304570594b3f3c3821cd0421b28a86bc4
Debjit <debjit20504@iiitd.ac.in>
2022-07-03 22:56:36 +0530

    added new solution

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Main {
 	public static void main(String[] args) {
 		power sd = new power();
-		var res = sd.solve(2,3);
+		var res = sd.solve(5,1);
 		System.out.println(res);
 	}
 }
--- power.java
+++ power.java
@@ -1,5 +1,14 @@
 public class power {
-	public int solve(int a, int b) {
-		return 
+	public int solve(int base, int exp) {
+		if(exp < 0) {
+			return -1;
+		}
+		if(base == 0 || exp == 1) {
+			return base;
+		}
+		if(exp == 0) {
+			return 1;
+		}
+		return base * solve(base, exp-1);
 	}
 }
