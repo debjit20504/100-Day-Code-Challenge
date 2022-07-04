3cbf67ee68afbe1f3c752238d531fb65c659e603
Debjit <debjit20504@iiitd.ac.in>
2022-07-04 18:08:57 +0530

    added new solution

--- gcd.java
+++ gcd.java
@@ -1,10 +1,8 @@
-class gcd {
-	public static void main(String[] args) {
-		public int solve(int a, int b) {
-			if(b == 0) {
-				return a;
-			}
-			return gcd(b, b%10);
+public class gcd {
+	public int solve(int a, int b) {
+		if(b == 0) {
+			return a;
 		}
+		return solve(b, a%b);
 	}
 }
