3fecfc7884ebc72ef3884a6e680f83916b547798
Debjit <debjit20504@iiitd.ac.in>
2022-07-14 15:45:25 +0530

    added new solution

--- gfg1.java
+++ gfg1.java
@@ -4,12 +4,12 @@ public class gfg1 {
 		for (i=n/2; i<=n; i++) {
 			for (j=2; j<=n; j=j*2) {
 				k = k + n/2;
-				System.out.print("k = "+k+", ");
-				System.out.print("j = "+j+", ");
+				// System.out.print("k = "+k+", ");
+				// System.out.print("j = "+j+", ");
 			}
-			System.out.print("i = "+i+", ");
-			System.out.println();
+			// System.out.print("i = "+i+", ");
+			// System.out.println();
 		}
-		return 0;
+		return 1/2;
 	}
 }
